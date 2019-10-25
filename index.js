b2a637fda97417e24235b1e9a9c5f97d1b6a6720
Sabrina Becker <sbecka8@gmail.com>
2019-10-24 23:48:15 -0400

    added css to app and made final modifications

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//Store Questions and Answers
 const STORE = [
     //1/10
     {
@@ -63,7 +63,7 @@ const STORE = [
     },    
 ];
 
-//Functions Below////////////////////////////////////////////////////////////
+///////////////////Functions Below////////////////////////////////////////////////////////////
 
 let questionNumber = 1;
 
@@ -71,7 +71,7 @@ function updateQuestionNumber() {
     //call this function or increase question number by 1 when Next button is clicked
     if (questionNumber < 10) {
         questionNumber ++;
-        console.log(`Question number is ${questionNumber}`);
+        //It works! console.log(`Question number is ${questionNumber}`);
     };
 };
 
@@ -81,13 +81,14 @@ function updateScore() {
     //increase by 1 for right answer
     if (overallScore < 10) {
         overallScore ++;
-        console.log(`Score is ${overallScore}`);
+        //It works! console.log(`Score is ${overallScore}`);
     };
 };
 
 function generateQuestionNumberAndScore() {
+    //create question number and score counter on page
     $("header").append(`
-    <section class="question-number-and-score js-question-number-and-score">
+    <section class="question-number-and-score js-question-number-and-score" role="banner">
         <h2 class="question-number js-question-number">Question: ${questionNumber}/10</h2>
         <h2 class="score js-score">Score: ${overallScore}/10</h2>
     </section>`);
@@ -96,20 +97,21 @@ function generateQuestionNumberAndScore() {
 let storeIndex = 0;
 
 function updateStoreIndex() {
-    
+    //go through the STORE array everytime this function is called
     if (storeIndex < STORE.length) {
         storeIndex ++;
-        console.log(`Store index is ${storeIndex}`);
+        //It works! console.log(`Store index is ${storeIndex}`);
     }; 
 };
 
 function generateQuestionAndAnswersForm() {
-
+    //create form for questions and answers
     $("main").append(`
-            <form id="js-question-page">
+            <form id="js-question-page" role="form">
                 <legend class="current-question js-current-question">${STORE[storeIndex].question}</legend>
+                <hr class="horizontal-top">
                 <fieldset>
-                    <section class="container js-container" role="contentinfo">
+                    <section class="container js-container" role="region">
                     </section>
                 </fieldset>
                 <button class="submit-button js-submit-button" type="submit" role="button">Submit</button>
@@ -127,18 +129,17 @@ function generateQuestionAndAnswersForm() {
 };
 
 function startQuiz() { 
-    console.log('startQuiz ran');
+    //working console.log('startQuiz ran');
     $('.js-start-button').on("click", function(){
         $('.js-start-page-content').hide();
         generateQuestionNumberAndScore();
         generateQuestionAndAnswersForm();
-        
-        
     });
 };
 
 function generateRightOrWrongAnswerPage() {
-    
+    //create right or wrong answer page based on user's answer
+    //find value of raido and get it's label's text to compare to right answer in array
     let radioValue = $("input[name=answer]:checked").val();
 
     let userAnswer = $(`label[for=${radioValue}]`).text();
@@ -149,18 +150,18 @@ function generateRightOrWrongAnswerPage() {
     let altText = null;
 
     if (userAnswer === STORE[storeIndex].rightAnswer) {
-
+        //right answer
         rightOrWrong = 'Right Answer!';
-        commentOnAnswer = 'Nice job!';
+        commentOnAnswer = "Nice job! You're bootiful!";
         pumpkinImage = 'happy-pumpkins.jpg';
         altText = 'Happy'
         updateScore(overallScore);
         $('.js-score').text(`Score: ${overallScore}/10`);
 
     } else {
-
+        //wrong answer
         rightOrWrong = 'Wrong Answer!';
-        commentOnAnswer = `The right answer is: ${STORE[storeIndex].rightAnswer}`;
+        commentOnAnswer = `The right answer is:<br>${STORE[storeIndex].rightAnswer}`;
         pumpkinImage = 'sadpumpkins.jpg';
         altText = 'Sad'
     };
@@ -168,54 +169,66 @@ function generateRightOrWrongAnswerPage() {
     $('main').append(`
     <form id="js-right-or-wrong-page">
         <legend>${rightOrWrong}</legend>
-        <img src="images/${pumpkinImage}" alt="${altText} Jack-o'-lanterns">
+        <img class="jack-o-lantern-pictures" src="images/${pumpkinImage}" alt="Two ${altText} Jack-o'-lantern Pumpkins" role="img">
         <p class="comment js-comment">${commentOnAnswer}</p>
         <button type="button" class="next-button js-next-button" role="button">Next</button>
     </form>`);
 };
 
 function submitAnswer() {
-    console.log(`submitAnswer ran`);
-    $('#js-question-page').on("submit", (function(event) { //submit from form element
-        event.preventDefault();
-        $('#js-question-page').hide();
-        generateRightOrWrongAnswerPage();
-        nextQuestion();//make next button work on its form
-    }));
-};
+    // working console.log(`submitAnswer ran`);
     //we need to focus on the input being selected
     //should submit form's answer and generate a text 'right or wrong answer!' on screen
     //Right Answer: Updates score by 1 and displays happy image
     //Wrong Answer: Displays sad image and the right answer
+    $('#js-question-page').on("submit", (function(event) { //submit from form element
+        event.preventDefault();
+        $('#js-question-page').hide();
+        generateRightOrWrongAnswerPage();
+        nextQuestion();//make 'next' button work on its form
+    }));
+};
+
 function finalScorePage() {
-
+    //create final score and resulting message page
     let commentOnScore = null;
+    let finalImage = null;
+    let imageAltText = null;
 
-    if (overallScore < 4) {
+    if (overallScore < 4) { //Bad Score (0-3)
         commentOnScore = "Do you even know what happens on October 31st? Go get a costume, you're missing out on the sweet treats!";
-    } else if (overallScore > 3 && overallScore < 8) {
-        commentOnScore = "Not bad! You almost know everything spooky about Halloween. Don't be a scaredy cat...try to beat your score.";
-    } else if (overallScore <= 10) {
-        commentOnScore = "You're an expert alright! Have a bootiful and spooktacular Halloween!"
+        finalImage = "pumpkin-misty.jpg";
+        imageAltText = "A cat about to lick an unhappy jack-o'-lantern and has a dialogue box above it asking, 'Can I eat this?'.";
+    } else if (overallScore > 3 && overallScore < 8) { //Good Score (4-7)
+        commentOnScore = "Not bad! You almost know everything spooky about Halloween.<br>Don't be a scaredy cat . . . <br>try to beat your score.";
+        finalImage = "ghost-behind-you.jpg";
+        imageAltText = "A close up of a gray cat's eyes and head with a small cartoon ghost in the background."
+    } else if (overallScore <= 10) { //Great Score (8-10)
+        commentOnScore = "You're an expert alright!<br>Have a bootiful and spooktacular Halloween!"
+        finalImage = "pumpkin-carving.jpg";
+        imageAltText = "A close-up view of a lighted up pumpkin in the dark with a cat sitting on a tree branch carved into the pumpkin."
     };
 
     $('main').append(`
-        <form id="final-score-page">
+        <form id="final-score-page" role="form">
             <p class="user-final-score">Your Score is: ${overallScore}/10</p>
-            <img src="images/ghost-behind-you.jpg" alt="Happy Image">
+            <img class="final-picture" src="images/${finalImage}" alt="${imageAltText}" role="img">
             <p class="final-comment">${commentOnScore}</p>
             <button class="restart-button js-restart-button" type="reset" role="button">Restart Quiz</button>
         </form>`);
 };
 
 function nextQuestion() {
-    console.log('nextQuestion ran');
+    // working console.log('nextQuestion ran');
+    //should generate the next question with answers
+    //updates question number and displays current score
+    //after the last question #10, the final score should be generated with a comment based on the final score earned
+
     $('#js-right-or-wrong-page').on("click", '.next-button', function() { //targeting submit from form
 
         if (questionNumber < 10) {
             updateQuestionNumber(questionNumber);
             $('.js-question-number').text(`Question: ${questionNumber}/10`);
-
             updateStoreIndex();
             $('#js-right-or-wrong-page').detach();
             $('#js-question-page').detach();
@@ -232,13 +245,11 @@ function nextQuestion() {
             restartQuiz();
         });
     };
-    //should generate the next question with answers
-    //updates question number and displays current score
-    //after the last question #10, the final score should be generated with a comment based on the final score earned
 };
 
 function restartQuiz() {
-    console.log('restartQuiz ran');
+    //working console.log('restartQuiz ran');
+    //should generate the start page
     $('#final-score-page').on("click", '.restart-button', function() {
         $('#final-score-page').detach();
         questionNumber = 1;
@@ -247,12 +258,9 @@ function restartQuiz() {
         $('.js-question-number-and-score').detach();
         $('.js-start-page-content').show();
     });
-    
-    //should generate the start page
 };
 
-
-//calls and runs the function
+//calls and runs the function startQuiz
 function createHalloweenQuiz() {
     startQuiz();
 };
